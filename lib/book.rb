c755e98c4fc154dfe0abf901ed8393704c03a53d
Nasser Sanou <nassersanou23@gmail.com>
2018-09-19 19:53:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,13 +6,20 @@ class Book
 
   def initialize(title)
     @title = title
+    @genres = genres
   end
 
+<<<<<<< HEAD
   def genre=(genre)
     @genre = genre
     GENRES << genre
   end
 
+=======
+  def genres
+    @genres = genres
+  end 
+>>>>>>> 930aab921565629d3287a8a9de7c6bfc4eafd358
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
